ba05bce82de43611f6acb9a89271f2d310e90255
cyantrestdev <ian.montanom@gmail.com>
2026-05-07 18:21:44 -0600

    Fix2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SUPABASE_URL:  'https://sxoauxhtjadeouezbcot.supabase.co',
-  SUPABASE_ANON: 'sb_publishable_Mq5E9gmJEu0FYDUS_5CCvQ_Htm_x0Vp',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4b2F1eGh0amFkZW91ZXpiY290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MjE4NTQsImV4cCI6MjA5Mjk5Nzg1NH0.wLnmF-oXv73SyYFHmYyoi3hAVPZdVRkkDJdS5hA7QIo',
   BREVO_KEY:     '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'
 };
